89ec9da2b5f51be2936c122fe07995ffe8fb4508
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2017-10-24 12:27:17 +0200

    updated

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,28 +1,36 @@
-class review {
+class review(
+String $user='mkoltsov',
+) {
+#$auser=validate_string($user)
   # this class should accept a parameter rather than having
   # the username hardcoded.
 
   # Uncomment and use this variable where appropriate
-#  $homedir = $user ? {
-#    'root'  => '/root',
-#    default => "/home/$user",
-#  }
+  $homedir = $user ? {
+    'root'  => '/root',
+    default => "/home/$user",
+  }
 
-  user { 'bob':
+  user {$user:
     ensure     => present,
     shell      => '/bin/bash',
     managehome => true,
   }
 
-  file { '/home/bob/.bashrc':
+  file {"/home/${user}/.bashrc":
     ensure => file,
-    owner  => 'bob',
-    group  => 'bob',
+    owner  => $user,
+    group  => $user,
     mode   => '0644',
     source => 'puppet:///modules/review/bashrc'
   }
 
   # add the proper resource to ensure that the Puppet agent is not running
   # in the background. How would you discover the service name?
+
+  service { 'puppet':
+  ensure => 'stopped',
+  enable => 'true',
+  }
 
 }
--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,23 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+resources {'host':
+purge => true,
+}
+host { 'master.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['puppet'],
+  ip           => '172.17.0.1',
+  target       => '/etc/hosts',
+}
+host { 'localhost':
+  ensure => 'present',
+  ip     => '127.0.0.1',
+  target => '/etc/hosts',
+}
+## Use your own IP, or the ::ipaddress fact
+host { 'mkoltsov.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['mkoltsov'],
+  ip           => '172.17.0.3',
+  target       => '/etc/hosts',
+}
 }
